317ea6fd76fbf6a5e681dcd17b0c1e8b314f26a5
BitsAmericas <bitsamericas@MacBook-Air-de-BitsAmericas.local>
2024-11-18 19:41:52 -0500

    Se actualiza repositorio 18 Nov 2024

--- android/src/main/java/com/juankmiloh/plugins/background/BackgroundModePlugin.java
+++ android/src/main/java/com/juankmiloh/plugins/background/BackgroundModePlugin.java
@@ -17,6 +17,7 @@ import android.content.pm.PackageManager;
 import android.content.pm.ServiceInfo;
 import android.os.Build;
 import android.os.IBinder;
+import android.os.PowerManager;
 import android.content.Context;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -164,16 +165,25 @@ public class BackgroundModePlugin extends Plugin {
 
     public static class AudioService extends Service {
         private static final String CHANNEL_ID = "ForegroundAudioServiceChannel";
+        private PowerManager.WakeLock wakeLock;
 
         private String notificationTitle = "Audio Service";
         private String notificationText = "Reproducción de audio en segundo plano";
         private String notificationIcon = "ic_launcher";
         private boolean showWhen = true;
 
+        @SuppressLint("WakelockTimeout")
         @Override
         public void onCreate() {
             super.onCreate();
             createNotificationChannel();
+
+            // Adquirir el WakeLock
+            PowerManager powerManager = (PowerManager) getSystemService(Context.POWER_SERVICE);
+            if (powerManager != null) {
+                wakeLock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "MyApp::AudioWakeLock");
+                wakeLock.acquire();
+            }
         }
 
         @Override
@@ -212,6 +222,11 @@ public class BackgroundModePlugin extends Plugin {
         @Override
         public void onDestroy() {
             super.onDestroy();
+
+            // Liberar el WakeLock
+            if (wakeLock != null && wakeLock.isHeld()) {
+                wakeLock.release();
+            }
 
             // Detener el servicio en primer plano y eliminar la notificación
             stopForeground(true);
